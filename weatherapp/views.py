ae54da004b944e48b34884cf7a0843e569fa374c
unknown <kumarpritam.baral2000@gmail.com>
2023-01-31 14:42:21 +0530

    first one

---
+++ weatherapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+import requests  
+
+
+def home(request):
+    if request.method=='POST':
+        city=request.POST.get('city')
+        API_KEY='ecde41743bdbf72c9abc21173c8988c3'
+        URL=f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
+        datadetail= requests.get(URL).json()
+        print(datadetail)
+
+        weather={
+             'city': city,  
+            "country_code": str(datadetail['sys']['country']),  
+            "coordinate": str(datadetail['coord']['lon']) + ' '  
+                            + str(datadetail['coord']['lat']),  
+            "temp": str("%.2f"%((datadetail['main']['temp'])-273)) + '  celsius',  
+            "pressure": str(datadetail['main']['pressure']),  
+            "humidity": str(datadetail['main']['humidity']),  
+
+        }
+
+    else:
+        weather={}
+
+
+    return render(request,'home.html',weather)
+
+
+
